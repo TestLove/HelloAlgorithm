db8e4e668296b2a9ca0361b7952585e5c35246b5
testlove <1923161393@qq.com>
2021-05-25 21:52:20 +0800

    写完了冒泡排序

---
+++ src/test/java/algorithm/sort/SortTest.java
@@ -0,0 +1,16 @@
+package algorithm.sort;
+
+import org.junit.Test;
+
+/**
+ * @author TestLove
+ * @version 1.0
+ * @date 2021/5/25 21:46
+ * @Description: 关于排序的各种测试
+ */
+public class SortTest {
+    @Test
+    public void bubbleSortTest() {
+
+    }
+}
